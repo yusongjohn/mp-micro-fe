589c4d8e5666436c04f7405821c62c5f5a2d7ca6
songyu <songyu@192.168.0.100>
2023-02-12 15:46:01 +0800

    chore: update readme content

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # feature
-- function(){return "this"} // 真正的globalThis，做不到完全隔离
+将多个应用合并为一个大的应用。测试案例参考当前项目的demo，执行demo下的test.js。
 
 # future
 - [ ] 目前实现的方式是，每个应用只能2M （如果一个子应用进一步拆分为多个子应用，那应该如何解决全局变量共享问题）
-- [ ] 独立分包方式接入
 
 # unit test framework
 - [ ] TODO
--- demo/compose.config.js
+++ demo/compose.config.js
@@ -2,22 +2,18 @@ const path = require('path');
 module.exports = {
     apps: [
         { // 不提供 namespace 就是主应用，其他为子应用（必须提供namespace）
-            name: 'a',
             projectDir: path.resolve(__dirname, 'miniprogramA'),
             // ignore
-            // 如何支持独立分包
         },
         {
-            name: 'b',
             projectDir: path.resolve(__dirname, 'miniprogramB'),
             namespace: 'b',
             // ignore
-            // 如何支持独立分包
         },
         {
-            name: 'c',
             projectDir: path.resolve(__dirname, 'miniprogramC'),
             namespace: 'c',
+            // ignore
         }
     ],
     workspace: __dirname,
